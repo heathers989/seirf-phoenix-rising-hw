a8cc3984a447738293c5cf9ae390191385ece923
Heather Spoelstra <heatherspoelstra@Heathers-Air.home>
2019-12-10 00:43:36 -0500

    five loops for practice

---
+++ current_week_homework_assignment_HeatherS/loops.js
@@ -0,0 +1,24 @@
+//log numbers 0-99 ascending 
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
+
+//log numbers 0-98 even numbers only, ascending
+for (let i = 0; i < 99; i+=2) {
+    console.log(i);
+}
+
+//log numbers 98-0 even numbers only, decending
+for (let i = 98; i >= 0; i-=2) {
+    console.log(i);
+}
+
+//log 49-72 ascending
+for (let i = 49; i < 73; i++) {
+    console.log(i);
+}
+
+//log multiples of 3
+for (let i = 3; i <= 90; i+=3) {
+    console.log(i);
+}
