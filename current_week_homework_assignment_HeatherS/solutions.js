bd0d7b9a33d97d39ce4521d32a53ae0a34f994a5
Heather Spoelstra <heatherspoelstra@Heathers-MacBook-Air.local>
2019-12-14 21:21:43 -0500

    sum array

--- current_week_homework_assignment_HeatherS/solutions.js
+++ current_week_homework_assignment_HeatherS/solutions.js
@@ -5,13 +5,24 @@
 
 //2. console.log() cannot produce the output value of a function, while return can.
 
-const checkPalindrome = (word) => {
-    const reversed = word.toLowerCase().split('').reverse().join('')
-    if(reversed === word.toLowerCase()) {
-        return true;
-   } else {
-        return false;
+//Palindrome
+// const checkPalindrome = (word) => {
+//     const reversed = word.toLowerCase().split('').reverse().join('')
+//     if(reversed === word.toLowerCase()) {
+//         return true;
+//    } else {
+//         return false;
+//     }
+//  };
+//  console.log(checkPalindrome("Noon"));
+//  console.log(checkPalindrome("Borscht"));
+
+ //Sum Array
+ const sumArray = (total) => {
+    let numbers = 0;
+    for(let i=0; i < total.length; i++) {
+        numbers += total[i];
     }
+    return numbers;
  };
- console.log(checkPalindrome("Noon"));
- console.log(checkPalindrome("Borscht"));
+ console.log(sumArray([1, 2, 3, 4, 5, 6]));
