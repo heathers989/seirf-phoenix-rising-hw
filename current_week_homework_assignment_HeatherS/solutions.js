664a6e78a1d442138bc7a3c6874561f936122b0c
Heather Spoelstra <heatherspoelstra@Heathers-MacBook-Air.local>
2019-12-14 20:59:08 -0500

    palindrome exercise

--- current_week_homework_assignment_HeatherS/solutions.js
+++ current_week_homework_assignment_HeatherS/solutions.js
@@ -4,3 +4,14 @@
 //  the parameter that is specified when the function is called.
 
 //2. console.log() cannot produce the output value of a function, while return can.
+
+const checkPalindrome = (word) => {
+    const reversed = word.toLowerCase().split('').reverse().join('')
+    if(reversed === word.toLowerCase()) {
+        return true;
+   } else {
+        return false;
+    }
+ };
+ console.log(checkPalindrome("Noon"));
+ console.log(checkPalindrome("Borscht"));
