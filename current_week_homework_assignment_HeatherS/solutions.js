dd33232d5219042e4699e16d77956223760fd597
Heather Spoelstra <heatherspoelstra@Heathers-MacBook-Air.local>
2019-12-14 20:10:14 -0500

    reorganized homework folders

---
+++ current_week_homework_assignment_HeatherS/solutions.js
@@ -0,0 +1,6 @@
+//Verbal Questions
+
+//1. The parameter is an input that modifies the function. The argument is the value of
+//  the parameter that is specified when the function is called.
+
+//2. console.log() cannot produce the output value of a function, while return can.
