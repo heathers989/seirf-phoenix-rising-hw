95d5e95f71dde650157233f998db9631e6430d6d
Heather Spoelstra <hmspoelstra@gmail.com>
2020-01-13 23:36:39 -0500

    created our enemy pizza rat

--- current_week_homework_assignment_HeatherS/donut_adventure/adventure.js
+++ current_week_homework_assignment_HeatherS/donut_adventure/adventure.js
@@ -20,5 +20,31 @@ class Hero {
     }
 }
 
-let dougie = new Hero('Dougie the donut')
+const dougie = new Hero('Dougie the donut')
 
+//Our Enemy
+
+class Enemy {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.weapons = {
+            pepperoniStars: 5,
+            cheeseGrease: 10
+        }
+        this.catchPhrases = ['I\'m YouTube famous', 'I\'m more dangerous than an uncovered sewer']
+    }
+    talkSmack() {
+        console.log(this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)])
+    }
+    announceHealth() {
+        console.log(`current HP is: ${this.health}`)
+    }
+    fight() {
+        console.log('I\'m gonna flatten you like a slice of pepperoni!')
+    }
+}
+
+const pizzaRat = new Enemy('Pizza Rat')
+
+console.log(pizzaRat)
