094ff3725d35ce7e2d6a9e07ace1d579dff09021
Heather Spoelstra <hmspoelstra@gmail.com>
2020-01-16 22:23:37 -0500

    I added more elements. My power grows with my dedication and hard work

--- current_week_homework_assignment_HeatherS/hogwarts/app.js
+++ current_week_homework_assignment_HeatherS/hogwarts/app.js
@@ -15,6 +15,23 @@ $( () => {
   $container.append($h1);
 
 
+//Year 2
+  const $h2 = $('<h2>').text("Heather");
+  console.log($h2)
+  $container.append($h2);
+
+  const $h3 = $('<h3>').text("Hufflepuff");
+  console.log($h3)
+  $container.append($h3);
+
+  const $h4 = $('<h4>').text("Broady");
+  $h4.addClass('toad')
+  console.log($h4)
+  $container.append($h4);
+
+  const $wand = $('<h4>').text("Hornbeam Wand with Dragon Heartstring Core");
+  console.log($wand)
+  $container.append($wand);
 
 
 });
