7dd5ad747009f635d592586e65fa803c3b8af1f2
Heather Spoelstra <hmspoelstra@gmail.com>
2020-01-16 22:03:22 -0500

    Commit 1 - I added my first element and it was fun

--- current_week_homework_assignment_HeatherS/hogwarts/app.js
+++ current_week_homework_assignment_HeatherS/hogwarts/app.js
@@ -1,10 +1,20 @@
 //opening $ function causes code to wait to execute until the document loads/is ready
 $( () => {
-  if (typeof $ == 'undefined'){
-    console.log('oops! I still have to link my jQuery properly!');
-  } else {console.log('I did it! I linked jQuery and this js file properly!')};
+  // if (typeof $ == 'undefined'){
+  //   console.log('oops! I still have to link my jQuery properly!');
+  // } else {console.log('I did it! I linked jQuery and this js file properly!')};
   
-  console.log($);
+  // console.log($);
+
+  //Year 1
+  const $container = $('#container');
+  console.log($container);
+
+  const $h1 = $('<h1>').text("Hogwarts");
+  console.log($h1)
+  $container.append($h1);
+
+
 
 
 });
