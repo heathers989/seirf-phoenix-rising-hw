aea66148a1c07fd698da2aa551cf38eca8da2a12
Heather Spoelstra <hmspoelstra@gmail.com>
2020-01-21 23:32:02 -0500

    chapter 9 complete - updated Gandalf

--- current_week_homework_assignment_HeatherS/w06d2_homework/scripts/fellowship.js
+++ current_week_homework_assignment_HeatherS/w06d2_homework/scripts/fellowship.js
@@ -223,8 +223,11 @@ const forgeTheFellowShip = () => {
 const theBalrog = () => {
 
   // 1. change the 'Gandalf' text to 'Gandalf the White'
+  const $gTW = $('.buddy');
+    // 2. add a class "the-white" to this element
+     $gTW.eq(0).text("Gandalf the White").addClass('the-white');
 
-  // 2. add a class "the-white" to this element
+
 
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
 
