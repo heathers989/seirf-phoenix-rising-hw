40e638926f6b3cbab452aaa50d632cac1be482be
Heather Spoelstra <hmspoelstra@gmail.com>
2020-02-10 22:00:46 -0500

    99 bottles mostly working

--- wk9_hw/server.js
+++ wk9_hw/server.js
@@ -17,7 +17,6 @@ app.get('/tip/:total/:tipPercentage', (req, res)=>{
 });
 
 //magic 8 ball
-
 const responseArr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 app.get('/magic/:phrase', (req, res)=>{
@@ -25,6 +24,7 @@ app.get('/magic/:phrase', (req, res)=>{
     let randomResponse = responseArr[Math.floor(Math.random() * responseArr.length)]
     res.send('Question is: ' + phrase + '? Response is: ' + randomResponse)
 });
+
 
 app.listen(port, () => {
 	console.log('app is running on port: ', port);
