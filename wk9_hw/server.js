48ca08bd48780b9715ad978c1edcb322c8a98245
Heather Spoelstra <hmspoelstra@gmail.com>
2020-02-10 20:19:08 -0500

    tip calculator express application created

--- wk9_hw/server.js
+++ wk9_hw/server.js
@@ -8,6 +8,14 @@ app.get('/greeting/:name', (req, res)=>{
     res.send('Hello there ' + name +'!')
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    let total = parseInt(req.params.total)
+    let tipPercentage = parseInt(req.params.tipPercentage)
+    let tipAmt = (total * tipPercentage) / 100
+    res.send('Tip amount is ' + tipAmt)
+});
+
+
 app.listen(port, () => {
 	console.log('app is running on port: ', port);
 });
