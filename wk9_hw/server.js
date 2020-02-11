690f7d4176e618a895d1877a15a7d779239053ca
Heather Spoelstra <hmspoelstra@gmail.com>
2020-02-10 20:35:35 -0500

    magic 8 ball express application created

--- wk9_hw/server.js
+++ wk9_hw/server.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var port = 3001;
 
-
+//greetings
 app.get('/greeting/:name', (req, res)=>{
     let name = req.params.name
     res.send('Hello there ' + name +'!')
 });
 
+//tip calculator
 app.get('/tip/:total/:tipPercentage', (req, res)=>{
     let total = parseInt(req.params.total)
     let tipPercentage = parseInt(req.params.tipPercentage)
@@ -15,6 +16,15 @@ app.get('/tip/:total/:tipPercentage', (req, res)=>{
     res.send('Tip amount is ' + tipAmt)
 });
 
+//magic 8 ball
+
+const responseArr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/:phrase', (req, res)=>{
+    let phrase = req.params.phrase;
+    let randomResponse = responseArr[Math.floor(Math.random() * responseArr.length)]
+    res.send('Question is: ' + phrase + '? Response is: ' + randomResponse)
+});
 
 app.listen(port, () => {
 	console.log('app is running on port: ', port);
