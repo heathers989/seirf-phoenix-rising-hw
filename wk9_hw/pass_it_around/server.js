5f4e018ded6f5958c9b8f78d1a8e52bf511dcf3d
Heather Spoelstra <hmspoelstra@gmail.com>
2020-02-10 21:59:58 -0500

    99 bottles mostly working

---
+++ wk9_hw/pass_it_around/server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var port = 3002;
+
+
+app.get('/', (req, res)=>{
+    res.send(`
+    <html>
+    <body>
+    <h1>99 bottles of beer on the wall</h1>
+    <a href="localhost:3002/98">take one down pass it around</a>
+    </body>
+    </html>`)
+});
+
+app.get('/:number_of_bottles', (req, res)=>{
+    let bottleNum = parseInt(req.params.number_of_bottles)
+    let takeOneDown = parseInt(bottleNum - 1)
+    if (bottleNum > 0) {
+        res.send(`
+        <html>
+        <body>
+        <h1>${bottleNum} bottles of beer on the wall</h1>
+        <a href="localhost:3002/${takeOneDown}">take one down pass it around</a>
+        </body>
+        </html>`)
+
+    } else if (bottleNum == 0) {
+        res.send(`<html>
+                  <body>
+                  <h1>${bottleNum} bottles of beer on the wall</h1>
+                  <a href="localhost:3002/">Start Over</a>
+                </body></html>`)
+    }
+    
+});
+
+
+app.listen(port, () => {
+	console.log('app is running on port: ', port);
+});
