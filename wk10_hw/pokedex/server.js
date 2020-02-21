bbcbcbfe54b7dc0ad71bbfa4620f6142a2153999
Heather Spoelstra <hmspoelstra@gmail.com>
2020-02-20 21:32:18 -0500

    index route is up and running

---
+++ wk10_hw/pokedex/server.js
@@ -0,0 +1,23 @@
+// Load express:
+const express = require('express');
+const app = express();
+const pokemon = require('./models/pokemon.js')
+const methodOverride = require("method-override");
+
+// Set the web server port:
+const port = 3000;
+
+// Load methodOverride middleware so you can make delete, put, and 
+// patch requests from web pages:
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+// INDEX:
+app.get("/pokedex", (req, res) => {
+    res.render("index.ejs", { data: pokemon });
+  });
+
+
+app.listen(port, () => {
+    console.log(`Pokedex app listening on port ${port}`)
+})
