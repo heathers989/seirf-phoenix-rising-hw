7e8dbd1ec8df2600e3ce7bb505993c712eac6f13
Heather Spoelstra <hmspoelstra@gmail.com>
2020-02-22 18:43:40 -0500

    added some styling, fixed edit and delete routes, added stats to new and show pages

--- wk10_hw/pokedex/server.js
+++ wk10_hw/pokedex/server.js
@@ -12,6 +12,9 @@ const port = 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
+//This lets the app use separate css files
+app.use(express.static('public'))
+
 //NEW
 app.get('/pokedex/new', (req, res) => {
     res.render('new.ejs')
@@ -26,17 +29,38 @@ app.post('/pokedex', (req, res) => {
     newPokemon.img = req.body.img
     newPokemon.type = [req.body.type]
     newPokemon.misc = {classification: req.body.classification}
+    newPokemon.stats = {hp: req.body.hp, attack: req.body.attack, defense: req.body.defense}
     pokemon.push(newPokemon)
     console.log(newPokemon)
     // const newPokemon = pokemon.push(req.body)
-    // console.log(newPokemon)
+    console.log(newPokemon)
 
+    res.redirect('/pokedex')
+})
+
+//EDIT
+app.get("/pokedex/:id/edit", (req, res) => {
+    res.render('edit.ejs', {
+        data: pokemon[req.params.id],
+        index: req.params.id
+    })
+})
+
+//UPDATE
+app.put('/pokedex/:id', (req, res) =>{
+    pokemon[req.params.id].name = req.body.name;
     res.redirect('/pokedex')
 })
 
 // SHOW
 app.get('/pokedex/:id', (req, res) => {
     res.render("show.ejs", {data: pokemon[req.params.id]})
+})
+
+//DELETE
+app.delete("/pokedex/:id", (req, res) => {
+    pokemon.splice(req.params.id, 1)
+    res.redirect('/pokedex')
 })
 
 // INDEX:
