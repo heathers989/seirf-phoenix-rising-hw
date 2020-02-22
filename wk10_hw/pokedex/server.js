d6c0b9922b0ca9df79a9b41c62fdb3f730b9c07b
Heather Spoelstra <hmspoelstra@gmail.com>
2020-02-22 00:35:24 -0500

    post route, new page and new show page working

--- wk10_hw/pokedex/server.js
+++ wk10_hw/pokedex/server.js
@@ -12,15 +12,38 @@ const port = 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
-// INDEX:
-app.get("/pokedex", (req, res) => {
-    res.render("index.ejs", { data: pokemon });
-  });
+//NEW
+app.get('/pokedex/new', (req, res) => {
+    res.render('new.ejs')
+    
+})
+
+//POST
+app.post('/pokedex', (req, res) => {
+    let newPokemon = {}
+    newPokemon.id = req.body.id
+    newPokemon.name = req.body.name
+    newPokemon.img = req.body.img
+    newPokemon.type = [req.body.type]
+    newPokemon.misc = {classification: req.body.classification}
+    pokemon.push(newPokemon)
+    console.log(newPokemon)
+    // const newPokemon = pokemon.push(req.body)
+    // console.log(newPokemon)
+
+    res.redirect('/pokedex')
+})
 
 // SHOW
 app.get('/pokedex/:id', (req, res) => {
     res.render("show.ejs", {data: pokemon[req.params.id]})
 })
+
+// INDEX:
+app.get("/pokedex", (req, res) => {
+    res.render("index.ejs", { data: pokemon });
+    console.log(pokemon[0].misc.classification)
+  });
 
 
 app.listen(port, () => {
