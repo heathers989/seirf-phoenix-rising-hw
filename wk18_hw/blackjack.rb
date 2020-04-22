1f68cf814aecfdd59c40fbc64e139df7a6d6e793
Heather Spoelstra <hmspoelstra@gmail.com>
2020-04-21 23:34:50 -0400

    finishing touches on ruby blackjack

--- wk18_hw/blackjack.rb
+++ wk18_hw/blackjack.rb
@@ -1,49 +1,4 @@
-# set up player class
-
-class Player
-    def initialize
-        @name = "name"
-        @bankroll = 1
-        @hand = []
-    end
-    def name
-        @name
-    end
-    def bankroll
-        @bankroll
-    end
-    def hand
-        @hand
-    end
-    def name=value
-        @name = value
-    end
-    def bankroll=value
-        @bankroll = value
-    end
-    def hand=value
-        @hand
-    end
-end
-#create array for deck
-deck = []
-
-#create class for card? May not be necessary.
-class Card
-    def initialize
-        @value = 2
-    end
-    def card_value
-        @value
-    end
-    def card_value=card_value
-        @value = card_value
-    end
-end
-
-#create an array for the number of cards in each suit 2-10 numbered, three additional 10s for the face cards, and 11 for ace
-cards_in_suit = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
+# other options for creating cards with suits
 #push the cards into the deck array as hashes
 # def hearts_cards
 # @cards_in_suit.each do |card|
@@ -77,6 +32,54 @@ cards_in_suit = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 # p @deck.length
 
+
+# set up player class
+
+class Player
+    def initialize
+        @name = "name"
+        @bankroll = 1
+        @hand = []
+    end
+    def name
+        @name
+    end
+    def bankroll
+        @bankroll
+    end
+    def hand
+        @hand
+    end
+    def name=value
+        @name = value
+    end
+    def bankroll=value
+        @bankroll = value
+    end
+    def hand=value
+        @hand
+    end
+end
+#create array for deck
+deck = []
+
+#create class for card? May not be necessary.
+class Card
+    def initialize value
+        @value = value
+    end
+    def card_value
+        @value
+    end
+    def card_value=card_value
+        @value = card_value
+    end
+end
+
+#create an array for the number of cards in each suit 2-10 numbered, three additional 10s for the face cards, and 11 for ace
+cards_in_suit = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
 i = 1
 for i in 1..4
     i+=1
@@ -109,70 +112,73 @@ play = true
 while play == true do
     round +=1
 
-if round > 13
-    puts "no more cards available!"
-    play = false
-end
-
-if human_player.bankroll <= 0
-    puts "You're out of money. Go home and rethink your life choices."
-    play = false
-end
-
-puts "Press any key to shuffle the deck..."
-$stdin.gets
-deck.shuffle!
-puts "Deck shuffled!"
-
-
-cards_drawn_human = deck.sample(2)
-deck.delete_at deck.index cards_drawn_human[0]
-deck.delete_at deck.index cards_drawn_human[1]
-
-cards_drawn_computer = deck.sample(2)
-deck.delete_at deck.index cards_drawn_computer[0]
-deck.delete_at deck.index cards_drawn_computer[1]
-
-# p deck.length
-
-human_player.hand.push(cards_drawn_human[0][:card_val], cards_drawn_human[1][:card_val])
-
-the_house.hand.push(cards_drawn_computer[0][:card_val], cards_drawn_computer[1][:card_val])
-
-
-human_hand_sum = human_player.hand[0] + human_player.hand[1]
-computer_hand_sum = the_house.hand[0] + the_house.hand[1]
-
-puts "Your cards are #{human_player.hand[0]} and #{human_player.hand[1]} for a total score of #{human_hand_sum}." 
-puts "How much would you like to bet? The minimum bet is 10, and maximum is your bankroll balance, which is currently at #{human_player.bankroll}."
-player_bet = $stdin.gets.to_i
-puts "Computer cards are #{the_house.hand[0]} and #{the_house.hand[1]} for a total score of #{computer_hand_sum}."
-
-if human_hand_sum == computer_hand_sum
-    puts "It is a tie! No bankrolls were affected."
-elsif human_hand_sum > computer_hand_sum || computer_hand_sum > 21
-    the_house.bankroll -= player_bet
-    human_player.bankroll += player_bet
-    puts "Congratulations #{human_player.name}, you win! Your bankroll is now #{human_player.bankroll}."
-elsif computer_hand_sum > human_hand_sum || human_hand_sum > 21
-    the_house.bankroll += player_bet
-    human_player.bankroll -= player_bet
-    puts "Sorry, you lose. Your bankroll is now #{human_player.bankroll}."
-end
-puts "Game over."
-puts "do you want to play another hand? type 'd' for deal and 'q' for quit."
-play_again = $stdin.gets.chomp
-if play_again == 'd'
-    play = true
-    human_player.hand.clear
-    the_house.hand.clear
-    elsif 
-        play_again == 'q'
+    if round > 13
+        puts "no more cards available!"
         play = false
-        p "thanks for playing!"
     end
 
+    if human_player.bankroll <= 0
+        puts "You're out of money. Go home and rethink your life choices."
+        play = false
+    end
+
+    puts "Press b to see your bankroll balance, or press the return key to continue."
+    check_bal = $stdin.gets.chomp
+    if check_bal == 'b'
+        puts "Your current bankroll balance is #{human_player.bankroll}"
+    end
+
+    puts "Press the return key to shuffle the deck..."
+    $stdin.gets
+    deck.shuffle!
+    puts "Deck shuffled!"
+
+
+    cards_drawn_human = deck.sample(2)
+    deck.delete_at deck.index cards_drawn_human[0]
+    deck.delete_at deck.index cards_drawn_human[1]
+
+    cards_drawn_computer = deck.sample(2)
+    deck.delete_at deck.index cards_drawn_computer[0]
+    deck.delete_at deck.index cards_drawn_computer[1]
+
+    # p deck.length
+
+    human_player.hand.push(cards_drawn_human[0][:card_val], cards_drawn_human[1][:card_val])
+
+    the_house.hand.push(cards_drawn_computer[0][:card_val], cards_drawn_computer[1][:card_val])
+
+
+    human_hand_sum = human_player.hand[0] + human_player.hand[1]
+    computer_hand_sum = the_house.hand[0] + the_house.hand[1]
+
+    puts "Your cards are #{human_player.hand[0]} and #{human_player.hand[1]} for a total score of #{human_hand_sum}." 
+    puts "How much would you like to bet? The default/minimum bet is 10, and maximum is your bankroll balance, which is currently at #{human_player.bankroll}."
+    player_bet = $stdin.gets.to_i
+    puts "Computer cards are #{the_house.hand[0]} and #{the_house.hand[1]} for a total score of #{computer_hand_sum}."
+
+    if human_hand_sum == computer_hand_sum
+        puts "It is a tie! No bankrolls were affected."
+    elsif human_hand_sum > computer_hand_sum || computer_hand_sum > 21
+        the_house.bankroll -= player_bet
+        human_player.bankroll += player_bet
+        puts "Congratulations #{human_player.name}, you win! Your bankroll is now #{human_player.bankroll}."
+    elsif computer_hand_sum > human_hand_sum || human_hand_sum > 21
+        the_house.bankroll += player_bet
+        human_player.bankroll -= player_bet
+        puts "Sorry, you lose. Your bankroll is now #{human_player.bankroll}."
+    end
+    puts "Game over."
+    puts "do you want to play another hand? type 'd' for deal and 'q' for quit."
+    play_again = $stdin.gets.chomp
+    if play_again == 'd'
+        play = true
+        human_player.hand.clear
+        the_house.hand.clear
+        elsif 
+            play_again == 'q'
+            play = false
+            p "thanks for playing!"
+        end
+
 end
-
-
-
