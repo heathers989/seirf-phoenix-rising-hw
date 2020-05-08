eb01dfd5a11761fef7065096eebad4ad8faafa1d
Heather Spoelstra <hmspoelstra@gmail.com>
2020-05-07 22:35:48 -0400

    react module and controller and configured cors

--- wk21_hw/dating_app/dating_app_client/src/App.js
+++ wk21_hw/dating_app/dating_app_client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Daters from './components/Hello.js'
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- wk21_hw/dating_app/dating_app_client/src/components/Hello.js
+++ wk21_hw/dating_app/dating_app_client/src/components/Hello.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 class Daters extends Component {
+    state = {
+        daters: []
+    }
+    componentDidMount(){
+        this.getDaters()
+    }
+    
+    getDaters () {
+        fetch('http://localhost:3000/users')
+        .then((response) => response.json())
+        .then( users => console.log(users))
+    }
     render() {
         return (
             <h1>Hello Dating World</h1>
