835f094e7c48e849bcb72a5605af928aeed08cea
Heather Spoelstra <hmspoelstra@gmail.com>
2020-05-07 21:55:43 -0400

    made react app

---
+++ wk21_hw/dating_app/dating_app_client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Daters from './components/Hello.js'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Daters />
+      </div>
+    )
+  }
+}
+
+export default App;
