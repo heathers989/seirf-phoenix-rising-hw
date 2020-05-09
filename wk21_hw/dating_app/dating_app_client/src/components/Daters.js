665eb3cc260ceee7e35755b7935b36757e5e4d62
Heather Spoelstra <hmspoelstra@gmail.com>
2020-05-08 22:52:07 -0400

    added some styling

--- wk21_hw/dating_app/dating_app_client/src/components/Daters.js
+++ wk21_hw/dating_app/dating_app_client/src/components/Daters.js
@@ -24,21 +24,25 @@ class Daters extends Component {
               return (
                 <div className="dater" key={dater.id}>
                  <img src={dater.img} alt={dater.name}/>
-                  <div><h3>Name: {dater.name} </h3>
-                  <h4>Starsign: {dater.starsign} </h4>
-                  <h4>Age: {dater.age} </h4>
-                  {dater.ltl
-                    ? <h5> Loves to laugh and have a good time</h5>
-                    : <h5> Hates laughing, does not like having a good time</h5>
-                  }
-                    </div>
+                 <div><h2>Name: {dater.name} </h2>
+                <h2>Starsign: {dater.starsign} </h2>
+                <h2>Age: {dater.age} </h2>
+                {dater.ltl
+                  ? <h3> Loves to laugh and have a good time</h3>
+                  : <h3> Hates laughing, does not like having a good time</h3>
+                }
                   </div>
+                </div>
                 
               )
             })}
           </div>
 
-          <main className="center">Let's make a match!</main>
+          <main className="center"><div>Let's make a match!</div>
+          {/* nonfunctional placeholders */}
+          <div className="datermid">Dater 1</div> <div className="datermid">Dater 2</div>
+          <div><button>CLICK TO MATCH!</button></div>
+          </main>
 
           <div className="right">
           {this.state.daters.filter((dater, index) => (index > 2))
@@ -46,12 +50,12 @@ class Daters extends Component {
             return (
               <div className="dater" key={dater.id}>
               <img src={dater.img} alt={dater.name}/>
-                <div><h3>Name: {dater.name} </h3>
-                <h4>Starsign: {dater.starsign} </h4>
-                <h4>Age: {dater.age} </h4>
+                <div><h2>Name: {dater.name} </h2>
+                <h2>Starsign: {dater.starsign} </h2>
+                <h2>Age: {dater.age} </h2>
                 {dater.ltl
-                  ? <h5> Loves to laugh and have a good time</h5>
-                  : <h5> Hates laughing, does not like having a good time</h5>
+                  ? <h3> Loves to laugh and have a good time</h3>
+                  : <h3> Hates laughing, does not like having a good time</h3>
                 }
                   </div>
                 </div>
